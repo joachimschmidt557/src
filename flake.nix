6741dd533c1e50b3b5d171189e94a7544d81017b
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-08-26 10:02:48 +0200

    Update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,14 +1,15 @@
 {
   description = "A small POSIX shell script which manages source directories for you.";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+  inputs.nixpkgs.url = "nixpkgs/nixos-21.05";
 
   outputs = { self, nixpkgs }: {
 
     defaultPackage.x86_64-linux =
       with import nixpkgs { system = "x86_64-linux"; };
       stdenv.mkDerivation {
-        name = "src";
+        pname = "src";
+        version = "master";
         src = self;
 
         makeFlags = [ "PREFIX=$(out)" ];
